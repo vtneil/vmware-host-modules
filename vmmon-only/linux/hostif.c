6f36ecead22aad8440b89ac7cc157741b4e2468c
Vivatsathorn Thitasirivit <vivatsathorn@outlook.co.th>
2025-08-25 22:29:38 +0700

    Kernel 6.16.3

--- vmmon-only/linux/hostif.c
+++ vmmon-only/linux/hostif.c
@@ -3415,7 +3415,7 @@ HostIF_SafeRDMSR(unsigned int msr,   // IN
    int err;
    u64 v;
 
-   err = rdmsrl_safe(msr, &v);
+   err = rdmsrq_safe(msr, &v);
    *val = (err == 0) ? v : 0;  // Linux corrupts 'v' on error
 
    return err;
